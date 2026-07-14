bbafebcc1b0c12f35a90e882c158841abcb0fc01
Praveen M <136952628+Praveen-M-18@users.noreply.github.com>
2026-07-14 14:24:16 +0530

    Register CarMake and CarModel in Django admin

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,1 +1,17 @@
+from django.contrib import admin
+from .models import CarMake, CarModel
 
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1
+
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name', 'description')
+
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')
+    list_filter = ['type', 'year', 'car_make']
+
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
